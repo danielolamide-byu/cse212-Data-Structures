79a45ce1b9cebe2a3e96c820275ffb891d67b168
danielolamide-byu <olamidedaniel874@gmail.com>
2026-03-21 15:49:04 +0100

    Stacks.

--- week02/code/PersonQueue.cs
+++ week02/code/PersonQueue.cs
@@ -1,35 +1,68 @@
-/// <summary>
-/// A basic implementation of a Queue
-/// </summary>
-public class PersonQueue
+public class PriorityQueue
 {
-    private readonly List<Person> _queue = new();
-
-    public int Length => _queue.Count;
+    private List<PriorityItem> _queue = new();
 
     /// <summary>
-    /// Add a person to the queue
+    /// Add a new value to the queue with an associated priority.  The
+    /// node is always added to the back of the queue regardless of 
+    /// the priority.
     /// </summary>
-    /// <param name="person">The person to add</param>
-    public void Enqueue(Person person)
+    /// <param name="value">The value</param>
+    /// <param name="priority">The priority</param>
+    public void Enqueue(string value, int priority)
     {
-        _queue.Insert(0, person);
+        var newNode = new PriorityItem(value, priority);
+        _queue.Add(newNode);
     }
 
-    public Person Dequeue()
+    public string Dequeue()
     {
-        var person = _queue[0];
-        _queue.RemoveAt(0);
-        return person;
+        if (_queue.Count == 0) // Verify the queue is not empty
+        {
+            throw new InvalidOperationException("The queue is empty.");
+        }
+
+        // Find the index of the item with the highest priority to remove
+        int highPriorityIndex = 0;
+        int highPriorityValue = _queue[0].Priority;
+        for (int index = 1; index < _queue.Count; index++)
+        {
+            if (_queue[index].Priority > highPriorityValue)
+            {
+                highPriorityIndex = index;
+                highPriorityValue = _queue[index].Priority;
+            }
+        }
+
+        // Remove and return the item with the highest priority
+        var value = _queue[highPriorityIndex].Value;
+        _queue.RemoveAt(highPriorityIndex);
+        return value;
     }
 
-    public bool IsEmpty()
-    {
-        return Length == 0;
-    }
-
+    // DO NOT MODIFY THE CODE IN THIS METHOD
+    // The graders rely on this method to check if you fixed all the bugs, so changes to it will cause you to lose points.
     public override string ToString()
     {
         return $"[{string.Join(", ", _queue)}]";
+    }
+}
+
+internal class PriorityItem
+{
+    internal string Value { get; set; }
+    internal int Priority { get; set; }
+
+    internal PriorityItem(string value, int priority)
+    {
+        Value = value;
+        Priority = priority;
+    }
+
+    // DO NOT MODIFY THE CODE IN THIS METHOD
+    // The graders rely on this method to check if you fixed all the bugs, so changes to it will cause you to lose points.
+    public override string ToString()
+    {
+        return $"{Value} (Pri:{Priority})";
     }
 }
